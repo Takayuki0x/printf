b22bc8cbd23c70e605f3b4e7284d93f9de9c2a51
Takayuki0x <122829295+Takayuki0x@users.noreply.github.com>
2023-08-18 16:24:37 +0100

    fixed test

--- tests/printfTesting.c
+++ tests/printfTesting.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-// Test what happens when you set formatting for digit but pass in a string
+/**
+ * Test what happens when you set formatting for digit but pass in a string
+*/
 
 int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
 {
 printf("%d", "string");
+return (0);
 }
